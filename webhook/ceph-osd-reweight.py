f2dd2111409bfe0280f3896f19c18ce603232900
smacphee-45d <smacphee@45drives.com>
2023-06-26 17:26:10 -0300

    Update ceph-osd-reweight.py

--- webhook/ceph-osd-reweight.py
+++ webhook/ceph-osd-reweight.py
@@ -5,61 +5,52 @@ import time
 import re
 
 def main():
-    #pass hostname command arg and use in json function
-    if len(sys.argv) != 2:
-        print("Only one hostname can be provided")
-        sys.exit(1)
     hostname = sys.argv[1]
-    if " " in hostname:
-        print("Spaces cannot be in hostnames")
-        sys.exit(1)
     child_osds = process_json(hostname)
-    #remove each found osd child from crushmap
     for osd, weight in child_osds.items():
         command = ["ceph", "osd", "crush", "remove", f"osd.{osd}"]
-        subprocess.run(command, capture_output=True, text=True)
-    #wait for all PGs to be in an acceptable state
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(f'Command output: {result.stdout}')
     wait_for_pg()
     print("PGs back to normal")
-    #readd all OSDs at original weights
     for osd, weight in child_osds.items():
         command = ["ceph", "osd", "crush", "add", f"osd.{osd}", str(weight), "root=default", f"host={hostname}"]
-        subprocess.run(command, capture_output=True, text=True)
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(f'Command output: {result.stdout}')
         command = ["ceph", "osd", "crush", "set-device-class", "hdd", f"osd.{osd}"]
-        subprocess.run(command, capture_output=True, text=True)
+        result = subprocess.run(command, capture_output=True, text=True)
+        print(f'Command output: {result.stdout}')
 
-#pull OSDs and OSD weights from ceph osd tree by hostname
+
 def process_json(hostname):
     osd_tree = subprocess.check_output(["ceph", "osd", "tree", "--format", "json-pretty"], stderr=subprocess.STDOUT).decode('utf-8')
     json_data = json.loads(osd_tree)
     osd_weights = {}
-    host_found = False
+    children = None
     for node in json_data['nodes']:
         if node['type'] == 'host' and node['name'] == hostname:
             children = node['children']
-            host_found = True
-        elif node['type'] == 'osd' and node['id'] in children:
+        elif children is not None and node['type'] == 'osd' and node['id'] in children: 
             osd_weights[node['id']] = node['crush_weight']
-    if not host_found:
-        print(f"The proided hostname is not found in ceph osd tree")
+    if not children:
+        print(f"No children OSDs found for host {hostname}")
         sys.exit(1)
+
     return osd_weights
 
-#wait for all PGs to not be active+undersized only
 def wait_for_pg():
     stop_state = "active+undersized"
-    #
-    valid_states = ["active+undersized+degraded", "active+undersized+backfill"]
+    acceptable_states = ["active+undersized+degraded", "active+undersized+backfill"]
     while True:
         result = subprocess.run(["ceph", "-s", "--format", "json-pretty"], capture_output=True, text=True)
         output = json.loads(result.stdout)
         pgs_by_state = output["pgmap"]["pgs_by_state"]
-        undersized_pg = False
+        undersized_found = False
         for pg in pgs_by_state:
-            if pg["state_name"] == stop_state and pg["state_name"] not in valid_states:
-                undersized_pg = True
+            if pg["state_name"] == stop_state and pg["state_name"] not in acceptable_states:
+                undersized_found = True
                 break
-        if not undersized_pg:
+        if not undersized_found:
             break
         time.sleep(5)
 
